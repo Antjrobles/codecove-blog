95b8de19776d52c74338f1e71bc62b5125fd20e2
Antonio Robles <antjrobles@gmail.com>
2023-09-23 01:12:50 +0200

    contentlayer 01:10:00

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
 import Image from 'next/image'
+import { allBlogs } from '../../.contentlayer/generated';
+
 
 export default function Home() {
+  console.log(allBlogs)
   return (
           <main className="flex flex-col items-center justify-center">
         <h1>Hello blog</h1>
--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -47,10 +47,10 @@ const Logo = () => {
 
   return (
     <Link href="/" className="flex items-center text-dark dark:text-light">
-        <div className=" w-12 md:w-16 rounded-full overflow-hidden  dark:border-gray  mr-2 md:mr-4">
+        <div className=" w-12 md:w-16 rounded-full overflow-hidden  dark:border-gray  mr-1 md:mr-2">
             <Image src={profileImg} alt="CodeCove logo" className="w-full h-auto rounded-full" sizes="20vw" priority />
         </div>
-      <span ref={ref} data-value="CodeCove" className="font-bold dark:font-semibold text-lg md:text-xl">CodeCove</span>
+      <span ref={ref} data-value="CodeCove" className="font-bold dark:font-semibold text-xl md:text-xl">CodeCove</span>
     </Link>
   )
 }
