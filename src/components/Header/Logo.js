2f910e79f0f160dacdd3a3c86798e40acac169b5
Antonio Robles <antjrobles@gmail.com>
2023-09-22 23:04:00 +0200

    Header 46:31

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,14 +1,12 @@
 
-
-
 import Image from "next/image"
 import Link from "next/link"
-import profileImg from '@/public/profile-img.png';
+import profileImg from '@/public/AROK.svg';
 
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark dark:text-light">
-        <div className=" w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
+        <div className=" w-12 md:w-16 rounded-full overflow-hidden  dark:border-gray  mr-2 md:mr-4">
             <Image src={profileImg} alt="CodeCove logo" className="w-full h-auto rounded-full" sizes="20vw" priority />
         </div>
         <span className="font-bold dark:font-semibold text-lg md:text-xl">CodeCove</span>
