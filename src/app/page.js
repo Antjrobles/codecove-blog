867dd6ac21bba801936139aea225030f0ec3d625
Antonio Robles <antjrobles@gmail.com>
2023-09-24 23:27:16 +0200

    01:20 <Image />

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import Image from 'next/image'
-import { allBlogs } from '../../.contentlayer/generated';
+import { allBlogs } from 'contentlayer/generated';
+import  HomeCoverSection  from '../components/Home/HomeCoverSection.js'
 
 
 export default function Home() {
-  console.log(allBlogs)
   return (
-          <main className="flex flex-col items-center justify-center">
-        <h1>Hello blog</h1>
+    <main className="flex flex-col items-center justify-center">
+      <HomeCoverSection blogs={allBlogs} />
     </main>
 
   )
